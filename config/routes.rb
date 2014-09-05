fe9c8e18d31d3ee011cb23552dc2ea6ddaab91ca
afifsohaili <mafif@booster.com>
2014-09-05 14:31:22 +0000

    First part done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
+  resources :photos
+  root :to => redirect('/photos')
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
